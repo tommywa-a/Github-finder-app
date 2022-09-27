53c6f14b3a0bde4cf60a2e5d8f469b2a8c5ad0fb
tommywa-a <tomiwa95@gmail.com>
2022-09-27 15:06:14 +0100

    Cleaning Up Actions File, & Use Of Axios

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,7 +4,7 @@ import { useParams, Link } from 'react-router-dom'
 import Spinner from '../components/layout/Spinner'
 import RepoList from '../components/repos/RepoList'
 import GithubContext from '../context/github/GithubContext'
-import { getUser, getUserRepos } from '../context/github/GithubActions'
+import { getUserAndRepos } from '../context/github/GithubActions'
 
 function User() {
   const {user, repos, loading, dispatch} = useContext(GithubContext)
@@ -14,12 +14,11 @@ function User() {
   useEffect(() => {
     dispatch({type: 'SET_LOADING'})
     const getUserData = async() => {
-      const UserData = await getUser(params.login)
-      dispatch ({type: 'GET_USER', payload: UserData})
+      const UserData = await getUserAndRepos(params.login)
+      dispatch ({type: 'GET_USER_AND_REPOS', payload: UserData})
 
-      const UserRepoData = await getUserRepos(params.login)
-      dispatch ({type: 'GET_REPOS', payload: UserRepoData})
-    }
+
+      }
 
     getUserData()
   }, [dispatch, params.login])
